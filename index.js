7d71e36f02c9a680d4874c810a8d87888c5a6338
Luan Motta <luanmotta97@hotmail.com>
2016-10-20 00:00:03 -0200

    Game Working

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ var HEIGHT = pixel*matrixHeight;
 var food;
 var matrix;
 var snake = {};
+var keyPressed;
 
-function generateSnake() {
-  snake.status = "ALIVE";
-  snake.direction = "undefined";
-  snake.blocks = [];
-  snake.blocks[0] = new SnakeBlock(matrixWidth/2, matrixHeight/2);
-  matrix[matrixWidth/2][matrixHeight/2] = new SnakeBlock(matrixWidth/2, matrixHeight/2);
+class SnakeBlock {
+  constructor(x = matrixWidth/2, y = matrixHeight/2) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Food {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 function reset() {
@@ -32,6 +39,90 @@ function init() {
   canvas.height = HEIGHT;
   reset();
   return setInterval(draw, 50);
+}
+
+function clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function generateFood() {
+
+  do {
+    var randomX = Math.floor(Math.random() * matrixWidth);
+    var randomY = Math.floor(Math.random() * matrixHeight);
+  } while (haveSnake(randomX, randomY));
+
+  food = new Food(randomX, randomY);
+  matrix[randomX][randomY] = food; 
+
+}
+
+function generateSnake() {
+  snake.status = "ALIVE";
+  snake.direction = "undefined";
+  snake.blocks = [];
+  snake.blocks[0] = new SnakeBlock();
+}
+
+function generateMatrix() {
+  matrix = new Array(matrixWidth);
+  matrix.fill(new Array(matrixHeight));
+}
+
+function haveSnake(x, y) {
+  var result = false;
+  snake.blocks.forEach(function (item) {
+    if (item.x == x && item.y == y) result = true;
+  });
+  return result;
+}
+
+function moveSnake() {
+  switch (snake.direction) {
+    case "UP":  /* Up arrow was pressed */
+      if (snake.blocks[0].y - 1 >= 0 && !(haveSnake(snake.blocks[0].x, snake.blocks[0].y - 1))) {
+        snake.blocks.unshift(new SnakeBlock(snake.blocks[0].x, snake.blocks[0].y - 1));
+      } else snake.status = "DEAD";
+      break;
+    case "DOWN":  /* Down arrow was pressed */
+      if (snake.blocks[0].y + 1 < matrixHeight && !(haveSnake(snake.blocks[0].x, snake.blocks[0].y + 1))) {
+        snake.blocks.unshift(new SnakeBlock(snake.blocks[0].x, snake.blocks[0].y + 1));
+      } else snake.status = "DEAD";
+      break;
+    case "LEFT":  /* Left arrow was pressed */
+      if (snake.blocks[0].x - 1 >= 0 && !(haveSnake(snake.blocks[0].x - 1, snake.blocks[0].y))) {
+        snake.blocks.unshift(new SnakeBlock(snake.blocks[0].x - 1, snake.blocks[0].y));
+      } else snake.status = "DEAD";             
+      break;  
+    case "RIGHT":  /* Right arrow was pressed */
+      if (snake.blocks[0].x + 1 < matrixWidth && !(haveSnake(snake.blocks[0].x + 1, snake.blocks[0].y))) {
+        snake.blocks.unshift(new SnakeBlock(snake.blocks[0].x + 1, snake.blocks[0].y));
+      } else snake.status = "DEAD";
+      break;
+    default:
+      snake.blocks.unshift(new SnakeBlock());
+  }
+}
+
+function doKeyDown(evt) {
+  if (keyPressed == false) {
+    switch (evt.keyCode) {
+      case 38: if ( snake.direction != "DOWN" )  snake.direction = "UP";    break;
+      case 40: if ( snake.direction != "UP" )    snake.direction = "DOWN";  break;
+      case 37: if ( snake.direction != "RIGHT" ) snake.direction = "LEFT";  break;
+      case 39: if ( snake.direction != "LEFT" )  snake.direction = "RIGHT"; break;
+      case 13: reset();
+    }
+  }
+  keyPressed = true;
+}
+
+function message(content, color) {
+  ctx.fillStyle = color;
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText(content, 32, 32);
 }
 
 function renderSnake() {
@@ -64,102 +155,30 @@ function renderFood(){
   ctx.stroke();
 }
 
-function generateFood() {
-
-  do {
-    var randomX = Math.floor(Math.random() * matrixWidth);
-    var randomY = Math.floor(Math.random() * matrixHeight);
-  } while (matrix[randomX][randomY] instanceof SnakeBlock);
-
-  food = new Food(randomX, randomY);
-  matrix[randomX][randomY] = food; 
-
-}
-
-function clear() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
-function moveSnake() {
-  switch (snake.direction) {
-    case "UP":  /* Up arrow was pressed */
-      if (snake.blocks[0].y - 1 >= 0){
-        snake.blocks[0].y -= 1;
-      } else snake.status = "DEAD";
-      break;
-    case "DOWN":  /* Down arrow was pressed */
-      if (snake.blocks[0].y + 1 < matrixHeight){
-        snake.blocks[0].y += 1;
-      } else snake.status = "DEAD";
-      break;
-    case "LEFT":  /* Left arrow was pressed */
-      if (snake.blocks[0].x - 1 >= 0){
-        snake.blocks[0].x -= 1;
-      } else snake.status = "DEAD";             
-      break;  
-    case "RIGHT":  /* Right arrow was pressed */
-      if (snake.blocks[0].x + 1 < matrixWidth){
-        snake.blocks[0].x += 1;
-      } else snake.status = "DEAD";
-  }
-}
-
-function doKeyDown(evt) {
-  switch (evt.keyCode) {
-    case 38: snake.direction = "UP";    break;
-    case 40: snake.direction = "DOWN";  break;
-    case 37: snake.direction = "LEFT";  break;
-    case 39: snake.direction = "RIGHT"; break;
-    case 13: reset();
-  }
-}
-
-function message(content, color) {
-  ctx.fillStyle = color;
-  ctx.font = "24px Helvetica";
-  ctx.textAlign = "left";
-  ctx.textBaseline = "top";
-  ctx.fillText(content, 32, 32);
-}
-
-function generateMatrix() {
-  matrix = new Array(matrixWidth);
-  matrix.fill(new Array(matrixHeight));
-}
-
-class SnakeBlock {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Food {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
 function renderMatrix() {
+  clear();
+  renderArena();
+  renderSnake();
   matrix.forEach(line => line.forEach(function(item) {
-
-    if (item instanceof SnakeBlock) {
-      renderSnake();
-    } else if (item instanceof Food) {
+    if (item instanceof Food) {
       renderFood();
     }
   }));
 }
     
 function draw() {
+  
+  keyPressed = false;
+
   if (snake.status == "ALIVE") {
     renderMatrix();
     moveSnake();
 
-    if (snake.blocks[0].x == food.x && snake.blocks[0].y == food.y) {
+    if ( snake.blocks[0].x == food.x && snake.blocks[0].y == food.y ) { 
       generateFood();
       renderFood();
+    } else {
+      snake.blocks.pop();
     }
   } else {
     message("You are dead!", "red");
